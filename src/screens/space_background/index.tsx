19d94631c598392b592a4817075be8896cdef75d
Nagge <yap@crtweb.ru>
2021-10-10 21:20:52 +0500

    publick path

--- src/screens/space_background/index.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from 'react'
-
-import * as styles from './styles.module.scss'
-
-const SpaceBackground: FC = ({ children }): JSX.Element => (
-  <div className={styles.space__background}>{children}</div>
-)
-
-export default SpaceBackground
--- typings/declarations.d.ts
+++
@@ -1,8 +0,0 @@
-declare module '*.svg'
-declare module '*.gif'
-declare module '*.png'
-declare module '*.jpg'
-declare module '*.jpeg'
-declare module '*.webp'
-declare module '*.css'
-declare module '*.scss'
